b33afafce9231e297dacaa3799ef759c2aa92e54
Michael Thornton <mbthornton@lbl.gov>
2024-01-02 09:52:03 -0800

    Add stub test

---
+++ tests/test_versioning.py
@@ -0,0 +1,10 @@
+"""
+test_versioning.py
+Tests for versioning of Workflow Execution objects
+"""
+
+def test_workflow_execution_versioning():
+    """
+    Test versioning of Workflow Execution objects
+    """
+    pass
