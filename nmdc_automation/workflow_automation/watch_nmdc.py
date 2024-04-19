80dc072d223e29078d58f51d1202e9c9beb9bf4a
Michal Babinski <mbabinski@lanl.gov>
2024-04-19 16:42:44 -0700

    watcher improvements
    - Allow map to be optional for annotation
    - Add timer for checkpointing
    - flake8

--- nmdc_automation/workflow_automation/watch_nmdc.py
+++ nmdc_automation/workflow_automation/watch_nmdc.py
@@ -11,6 +11,7 @@ from nmdc_automation.api import NmdcRuntimeApi
 from nmdc_automation.config import Config
 from .wfutils import WorkflowJob as wfjob
 from .wfutils import NmdcSchema, _md5
+from time import time
 
 logger = logging.getLogger(__name__)
 
@@ -19,6 +20,7 @@ class Watcher:
     def __init__(self, site_configuration_file):
         self._POLL = 20
         self._MAX_FAILS = 2
+        self._CHECKPOINT_WAIT = 30
         self.should_skip_claim = False
         self.config = Config(site_configuration_file)
         self.client_id = self.config.client_id
@@ -28,6 +30,8 @@ class Watcher:
         self.stage_dir = self.config.stage_dir
         self.raw_dir = self.config.raw_dir
         self.jobs = []
+        self.modifed = False
+        self.next_checkpoint = 0
         self.runtime_api = NmdcRuntimeApi(site_configuration_file)
         self._ALLOWED = self.config.allowed_workflows
 
@@ -67,6 +71,8 @@ class Watcher:
         data = {"jobs": jobs}
         with open(self.state_file, "w") as f:
             json.dump(data, f, indent=2)
+        self.modified = False
+        self.next_checkpoint = time() + self._CHECKPOINT_WAIT
 
     def cycle(self):
         self.restore()
@@ -80,7 +86,8 @@ class Watcher:
             try:
                 self.cycle()
             except (IOError, ValueError, TypeError, AttributeError) as e:
-                logger.exception(f"Error occurred during cycle: {e}", exc_info=True)
+                msg = f"Error occurred during cycle: {e}"
+                logger.exception(msg, exc_info=True)
             sleep(self._POLL)
 
     def find_job_by_opid(self, opid):
@@ -93,6 +100,7 @@ class Watcher:
             return
         self.create_or_use_existing_job(new_job, opid, common_workflow_id)
         self.jobs[-1].cromwell_submit(force=force)
+        self.modified = True
 
     def create_or_use_existing_job(self, new_job, opid, common_workflow_id):
         job = self.find_job_by_opid(opid)
@@ -112,6 +120,7 @@ class Watcher:
                 activity_id=new_job["config"]["activity_id"],
             )
             self.jobs.append(job)
+        self.modified = True
 
     def refresh_remote_jobs(self):
         """
@@ -173,7 +182,8 @@ class Watcher:
         )
         activity_inputs = [dobj["id"] for dobj in job.input_data_objects]
 
-        self.create_activity_record(job, act_id, activity_inputs, output_ids, schema)
+        self.create_activity_record(job, act_id, activity_inputs, output_ids,
+                                    schema)
 
         self.write_metadata_if_not_exists(metadata, outdir)
 
@@ -181,18 +191,37 @@ class Watcher:
         nmdc_database_obj_dict = json.loads(nmdc_database_obj)
         resp = self.runtime_api.post_objects(nmdc_database_obj_dict)
         logger.info(f"Response: {resp}")
+        if 'detail' in resp:
+            self._write_error(act_id, resp, nmdc_database_obj_dict)
+            return resp
 
         job.done = True
         resp = self.runtime_api.update_op(job.opid, done=True, meta=metadata)
 
         return resp
 
-    def generate_data_objects(self, job, job_outs, outdir, informed_by, act_id, schema):
+    def _write_error(self, act_id, err_msg, obj_data):
+        err_file = f"{act_id}.err"
+        if os.path.exists(err_file):
+            return None
+        # logger.error(json.dumps(nmdc_database_obj_dict, indent=2))
+        with open(err_file, "w") as f:
+            f.write(str(err_msg))
+            f.write("\n{'='*40}\n")
+            f.write(json.dumps(obj_data, indent=2))
+            logger.error(f"Posting of {act_id} failed. Error File: {err_file}")
+        return err_file
+
+    def generate_data_objects(self, job, job_outs, outdir, informed_by, act_id,
+                              schema):
         output_ids = []
         prefix = job.workflow_config["input_prefix"]
 
         for product_record in job.outputs:
             outkey = f"{prefix}.{product_record['output']}"
+            if outkey not in job_outs:
+                logger.warn(f"Missing {outkey}. Continuing")
+                continue
             full_name = job_outs[outkey]
             file_name = os.path.basename(full_name)
             new_path = os.path.join(outdir, file_name)
@@ -216,7 +245,8 @@ class Watcher:
 
         return output_ids
 
-    def create_activity_record(self, job, act_id, activity_inputs, output_ids, schema):
+    def create_activity_record(self, job, act_id, activity_inputs, output_ids,
+                               schema):
         activity_type = job.activity_templ["type"]
         name = job.activity_templ["name"].replace("{id}", act_id)
         omic_id = job.workflow_config["was_informed_by"]
@@ -246,10 +276,15 @@ class Watcher:
                 status = job.check_status()
                 if status == "Succeeded" and job.opid:
                     self.process_successful_job(job)
+                    self.modified = True
                 elif status == "Failed" and job.opid:
                     self.process_failed_job(job)
+                    self.modified = True
+            if self.modified and time() > self.next_checkpoint:
+                self.job_checkpoint()
 
-        self.job_checkpoint()
+        if self.modified:
+            self.job_checkpoint()
 
     def process_successful_job(self, job):
         self.post_job_done(job)
@@ -257,4 +292,5 @@ class Watcher:
     def process_failed_job(self, job):
         if job.failed_count < self._MAX_FAILS:
             job.failed_count += 1
+            logger.warn("Resubmitting failed job")
             job.cromwell_submit()
