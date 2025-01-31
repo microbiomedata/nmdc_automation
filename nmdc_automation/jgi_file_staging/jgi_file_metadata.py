eca5342019b40d9f978563a552ee9e933232f44e
aclum <aclum@users.noreply.github.com>
2025-01-30 17:44:17 -0800

    Update jgi_file_metadata.py

--- nmdc_automation/jgi_file_staging/jgi_file_metadata.py
+++ nmdc_automation/jgi_file_staging/jgi_file_metadata.py
@@ -11,8 +11,8 @@ import argparse
 from pathlib import Path
 from itertools import chain
 
-from mongo import get_mongo_db
-from models import Sample, SequencingProject
+from nmdc_automation.jgi_file_staging.import.mongo  get_mongo_db
+from nmdc_automation.jgi_file_staging.import.models import Sample, SequencingProject
 from typing import List
 from pydantic import ValidationError
 
