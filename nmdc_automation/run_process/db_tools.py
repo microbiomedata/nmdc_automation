72ef7bebf58ae61b4470bb2de8e17fc3a4cabc3c
Michael Thornton <mbthornton@lbl.gov>
2025-04-08 09:05:02 -0700

    check in

--- nmdc_automation/run_process/db_tools.py
+++ nmdc_automation/run_process/db_tools.py
@@ -6,6 +6,7 @@ import json
 import os
 
 import click
+import requests
 
 from nmdc_automation.config import SiteConfig
 from nmdc_automation.api import NmdcRuntimeApi
@@ -177,6 +178,123 @@ def fix_data_object_urls(config_file, update_db):
         print(data_objects_json)
         return
 
+
+@cli.command()
+@click.argument("site_config_file", type=click.Path(exists=True))
+def report_study_progress(site_config_file):
+    """
+    Generate a study progress report for the NMDC database.
+
+    Arguments:
+
+    - site_config_file: YAML file with site configuration
+
+    The report will be generated in the current directory.
+    """
+    site_config = SiteConfig(site_config_file)
+    username = site_config.username
+    password = site_config.password
+
+
+    headers = {'accept': 'application/json', 'Authorization': f'Basic {username}:{password}'}
+    # Get studies
+    study_params = {
+        'max_page_size': '500'
+    }
+    study_response = requests.get(
+        'https://api.microbiomedata.org/nmdcschema/study_set', params=study_params, headers=headers
+    )
+    if study_response.status_code != 200:
+        logger.error(f"Error in response: {study_response.status_code}")
+        return
+
+    studies = study_response.json().get("resources", [])
+    logger.info(f"Found {len(studies)} studies")
+
+
+    report = {}
+    # Iterate over studies and get the data generations
+    for study in studies:
+        study_name = study.get("name", None)
+        if not study_name:
+            # get the first 50 characters of description
+            study_name = study.get("description", None) or ""
+        logger.info(f"Study: {study['id']}, Name: {study_name}")
+
+        report[study['id']] = {
+            "name": study_name,
+            "data_generations": {}
+        }
+
+        # Get the data generations for the study
+        data_generation_params = {
+            'filter': f'{{"associated_studies": "{study["id"]}"}}',
+            'max_page_size': '1000'
+        }
+        data_generation_response = requests.get(
+            'https://api.microbiomedata.org/nmdcschema/data_generation_set', params=data_generation_params, headers=headers
+        )
+
+        if data_generation_response.status_code != 200:
+            logger.error(f"Error in response: {data_generation_response.status_code}")
+            return
+        data_generations = data_generation_response.json().get("resources", [])
+        logger.info(f"Found {len(data_generations)} data generations for study {study['id']}")
+        # Organized by type and analyte category
+        for dg in data_generations:
+            dg_type = dg['type']
+            analyte_category = dg['analyte_category']
+            if dg_type not in report[study['id']]['data_generations']:
+                report[study['id']]['data_generations'][dg_type] = {}
+            if analyte_category not in report[study['id']]['data_generations'][dg_type]:
+                report[study['id']]['data_generations'][dg_type][analyte_category] = []
+
+
+
+            # Get workflow_execution(s) that was_informed_by the data generation
+            data_gen_workflows= {dg['id']: []}
+            workflow_execution_params = {
+                'filter': f'{{"was_informed_by": "{dg["id"]}"}}',
+                'max_page_size': '1000'
+            }
+            workflow_execution_response = requests.get(
+                'https://api.microbiomedata.org/nmdcschema/workflow_execution_set', params=workflow_execution_params, headers=headers
+            )
+            if workflow_execution_response.status_code != 200:
+                logger.error(f"Error in response: {workflow_execution_response.status_code}")
+                return
+            workflow_executions = workflow_execution_response.json().get("resources", [])
+            logger.info(f"Found {len(workflow_executions)} workflow executions for data generation {dg['id']}")
+            # Count the number of workflow executions by type and version
+            for we in workflow_executions:
+                we_type = we.get('type', 'UnknownType')
+                # version may not be present
+                we_version = we.get('version', 'UnknownVersion')
+
+                data_gen_workflows[dg['id']].append((we_type, we_version))
+
+            report[study['id']]['data_generations'][dg_type][analyte_category].append(data_gen_workflows)
+
+    # Summarize the report -
+    # Study ID, Study Name, Data Generation Type, Analyte Category, Number of Data Generations, Min/Max/Average Workflow Executions
+    # Report format:
+    # Study ID, Study Name, Data Generation Type, Analyte Category, Number of Data Generations, Min/Max/Average Workflow Executions
+    report_summary = []
+    for study_id, study in report.items():
+         for sstudy_name, data_generations in study['data_generations'].items():
+            for dg_type, analyte_categories in data_generations.items():
+                for analyte_category, data_gen_workflows in analyte_categories.items():
+                    num_data_generations = len(data_gen_workflows)
+                    min_workflow_executions = min([len(workflows) for workflows in data_gen_workflows])
+                    max_workflow_executions = max([len(workflows) for workflows in data_gen_workflows])
+                    avg_workflow_executions = sum([len(workflows) for workflows in data_gen_workflows]) / num_data_generations
+                    report_summary.append((study_id, study['name'], dg_type, analyte_category, num_data_generations, min_workflow_executions, max_workflow_executions, avg_workflow_executions))
+    # Write the report to a CSV file
+    report_file = os.path.join(os.getcwd(), "study_progress_report.csv")
+    with open(report_file, "w") as f:
+        f.write("Study ID, Study Name, Data Generation Type, Analyte Category, Number of Data Generations, Min Workflow Executions, Max Workflow Executions, Average Workflow Executions\n")
+        for row in report_summary:
+            f.write(",".join([str(x) for x in row]) + "\n")
 
 
 
