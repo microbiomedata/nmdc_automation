a54108de989c9b8307ebc785ac6919ce57cf26d7
shreddd <shreyas@gmail.com>
2025-06-10 20:59:07 -0700

    jat import error handling

--- data_management/jamo_import.sh
+++ data_management/jamo_import.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-# Usage: jamo_import.sh <path_to_metadata_dir>
+# Usage: jamo_import.sh [--dev] <path_to_metadata_dir>
 
 # Example:
 # cd /global/cfs/cdirs/m3408/jamo_metadata ;
 # jamo_import.sh /global/cfs/cdirs/m3408/jamo_metadata/metadata_files 2>&1 | tee jamo_import.log
+
+# Parse command line arguments
+DEV_FLAG=false
+METADATA_DIR=""
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --dev)
+      DEV_FLAG=true
+      shift
+      ;;
+    *)
+      METADATA_DIR="$1"
+      shift
+      ;;
+  esac
+done
 
 declare -A wf_dict=(
   ["wfmag"]="nmdc_mags_analysis"
@@ -16,14 +33,29 @@ declare -A wf_dict=(
   ["wfmp"]="nmdc_metaproteomics_analysis"
 )
 
-cd $1
-module load jamo
+cd $METADATA_DIR
 
-# todo check for workflow_execution record in jamo
+# if dev flag is set run module load jamo/dev
+if [ "$DEV_FLAG" = true ]; then
+  module load jamo/dev
+else
+  module load jamo
+fi
 
+# TODO: check for workflow_execution record in jamo
 for file in metadata*.json; do
   wf=$(echo "$file" | cut -d':' -f3 | cut -d'-' -f1)
-  jat import ${wf_dict[$wf]} $file
-  mv $file ${file}.done
-  echo "Imported $file"
+  # if dev flag is set run use dev/${wf_dict[$wf]}
+  if [ "$DEV_FLAG" = true ]; then
+    jat import dev/${wf_dict[$wf]} $file
+  else
+    jat import ${wf_dict[$wf]} $file
+  fi
+
+  if [ $? -eq 0 ]; then
+    mv $file ${file}.done
+    echo "Imported $file"
+  else
+    echo "Error: Failed to import $file"
+  fi
 done
