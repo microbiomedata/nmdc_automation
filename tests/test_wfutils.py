30c0096b267c32ca989efba4839c03508eebf7fb
Jasmyn Pangilinan <JLPangilinan@JLPangilinan-M24.local>
2025-08-11 15:37:18 -0700

    Adding support for dev jaws tags with a unit test to test the config and another to test the jawsrunner submitting a job

--- tests/test_wfutils.py
+++ tests/test_wfutils.py
@@ -445,3 +445,28 @@ def test_jaws_workflow_execution_record_has_ended_at_time(fixture_pair, site_con
     wfe = wfj.make_workflow_execution([])
     assert wfe.started_at_time
     assert wfe.ended_at_time
+
+
+@mock.patch("nmdc_automation.workflow_automation.wfutils.WorkflowStateManager.generate_submission_files")
+def test_jaws_job_runner_submit_job_new_job(mock_generate_submission_files, site_config, fixtures_dir, mock_jaws_api):
+    """
+        Test submitting a job through the JAWS job runner and set up of the jaws tag based on the config
+    """
+    mock_generate_submission_files.return_value = {
+        "wdl_file": "workflowSource",
+        "sub": "workflowDependencies",
+        "inputs": "workflowInputs"
+    }
+    # A new workflow job that has not been submitted - it has a workflow state
+    # but no job metadata
+    wf_state = json.load(open(fixtures_dir / "mags_workflow_state.json"))
+    #wfj = WorkflowJob(site_config, wf_state, job_metadata, jaws_api=mock_jaws_api)
+    wf_state['last_status'] = None # simulate a job that has not been submitted
+    wf_state['jaws_jobid'] = None # simulate a job that has not been submitted
+    wf_state['done'] = False # simulate a job that has not been submitted
+
+    wf_state_manager = WorkflowStateManager(wf_state)
+    job_runner = JawsRunner(site_config, wf_state_manager, mock_jaws_api, dry_run=True)
+    jobid = job_runner.submit_job()
+    assert site_config.env == "dev"
+    assert jobid
