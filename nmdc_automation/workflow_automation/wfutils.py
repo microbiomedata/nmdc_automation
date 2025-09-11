90c2bc9aeaeba51917a9cca7dd0c350ddd2a63af
Kaitlyn Li <kli@lanl.gov>
2025-09-11 17:34:43 -0600

    time delay works on dev

--- nmdc_automation/workflow_automation/wfutils.py
+++ nmdc_automation/workflow_automation/wfutils.py
@@ -198,6 +198,7 @@ class JawsRunner(JobRunnerABC):
                 )
                 self.job_id = response['run_id']
                 logger.info(f"Submitted job {response['run_id']}")
+                logger.info(f"Starting wait period to allow job to be registered in jaws")
                 time.sleep(60)  # wait for a minute to allow the job to be registered in jaws
             else:
                 logger.info(f"Dry run: skipping jaws job submission")
