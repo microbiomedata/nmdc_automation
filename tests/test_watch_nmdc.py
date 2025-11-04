270ee72ac85bdd857b58f452c628a07c98ef80aa
Kaitlyn Li <kli@lanl.gov>
2025-11-04 14:18:33 -0700

    added a file to straighten out mock logic

--- tests/test_watch_nmdc.py
+++ tests/test_watch_nmdc.py
@@ -308,7 +308,7 @@ def test_job_manager_get_finished_jobs(site_config, initial_state_file_1_failure
     assert len(jm.job_cache) == 2
 
     # add a failed job
-    failed_job_state = json.load(open(fixtures_dir / "failed_job_state_2.json"))
+    failed_job_state = json.load(open(fixtures_dir / "failed_job_state_2_v2.json")) #replaced failed_job_state_2.json because it held the same job as agent_state_1_failure.json
     assert failed_job_state
     failed_job = WorkflowJob(site_config, failed_job_state)
     assert failed_job.job_status.lower() == "failed"
@@ -326,24 +326,21 @@ def test_job_manager_get_finished_jobs(site_config, initial_state_file_1_failure
 
     # Mock requests for job status
     with requests_mock.Mocker() as m:
-        # why aren't we just reading the values from the job states directly? 
-        # should specify in comments if we're mocking different values than in the files.
         # Mock the initial job status from mags_workflow_state.json, last_status of Succeeded
         m.get(
             "http://localhost:8088/api/workflows/v1/8f4e1a0b-ca5c-455b-9db6-30957bcf4b4a/status",
             json={"status": new_job_state["last_status"]}
             )
-        # Mock the successful job status from agent_state_1_failure.json and failed_job_state_2.json. 
-        # "last_status": "Failed", with fail count 2 for the file _2, so i guess it's just updating? 
+        # Mock the failed job status from agent_state_1_failure.json. 
+        # updated to only have one occurrence in cache, no longer returns two Succeeded
         m.get(
             "http://localhost:8088/api/workflows/v1/9492a397-eb30-472b-9d3b-abc123456789/status",
             json={"status": "Succeeded"}
             )
-        # Mock the failed job status, not sure where this job id came from, just using a placeholder?
-        # this is not failing as it should be? Maybe make a file for this, replacing failed_job_state_2.json?
+        # Mock the failed job status from failed_job_state_2_v2.json. Used to not point to anything or update anything
         m.get(
             "http://localhost:8088/api/workflows/v1/12345678-abcd-efgh-ijkl-9876543210/status",
-            json={"status": "Failed"}
+            json={"status": failed_job_state["last_status"]}
             )
         # Mock for the manifest job, last status Submitted, mocking as Succeeded for sake of test
         m.get(
